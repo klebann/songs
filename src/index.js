28e254687554594d475e46d8fec23df752410930
Jakub Kleban <jakub.kleban@enovation.ie>
2022-04-25 17:13:04 +0200

    Building a list with Redux Data and calling action

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import App from './components/App';
+import reducers from './reducers';
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App tab="home" />);
+root.render(
+  <Provider store={createStore(reducers)}>
+    <App tab="home" />
+  </Provider>
+);
